2dbde0daf74218f29e82214fa7ecd1f4d9dd7c15
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-17 02:12:58 +0900

    login view form

--- app/member/forms.py
+++ app/member/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth import authenticate, login
+from django.core.exceptions import ValidationError
 
 
 class LoginForm(forms.Form):
@@ -13,3 +15,18 @@ class LoginForm(forms.Form):
             'placeholder': '비밀번호'
         }
     ))
+
+    # is_valid() 호출시 clean() 자동 호출. clean() 함수를 오버라이딩
+    def clean(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise ValidationError("아이디 혹은 비밀번호를 잘못 입력하셨 습니다.")
+        return self.cleaned_data
+
+    def login(self,request):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        user = authenticate(request, username=username, password=password)
+        login(request,user)
