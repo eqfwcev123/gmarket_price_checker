c343af63fc97b11b2e0ecc9f29d3fd47c37b6b71
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-16 18:28:24 +0900

    changed fields for models

--- app/shoppingList/models.py
+++ app/shoppingList/models.py
@@ -7,8 +7,8 @@ class Item(models.Model):
     item_name = models.CharField(max_length=100)
     item_original_price = models.CharField(max_length=100)
     item_discount_price = models.CharField(max_length=100)
-    item_discount_rate = models.IntegerField()
-    item_image = models.ImageField()
+    item_discount_rate = models.CharField(max_length=100)
+    item_image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.item_name, self.item_original_price, self.item_discount_price
