443dd7d545b4b0270acddce4494666f515e5d11b
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-17 02:11:04 +0900

    added user field to model

--- app/shoppingList/models.py
+++ app/shoppingList/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+from member.models import User
+
 
 class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     item_name = models.CharField(max_length=100)
-    item_original_price = models.CharField(max_length=100)
-    item_discount_price = models.CharField(max_length=100)
-    item_discount_rate = models.CharField(max_length=100)
+    item_original_price = models.CharField(max_length=100, blank=True)
+    item_discount_price = models.CharField(max_length=100, blank=True)
+    item_discount_rate = models.CharField(max_length=100, blank=True)
     item_image = models.ImageField(blank=True)
 
     def __str__(self):
