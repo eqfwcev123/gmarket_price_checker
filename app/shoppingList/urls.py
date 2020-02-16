09d2478315952b91fbad92791edade4de9ae233f
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-17 02:10:48 +0900

    removed initial url

--- app/shoppingList/urls.py
+++ app/shoppingList/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from shoppingList.views import shoppingList, data_crawler
+from shoppingList.views import shoppingList
 
 app_name = 'shop'
 urlpatterns = [
     path('shoppinglist/', shoppingList, name="shopping-list"),
-    path('all_items/<str:keyword>/', data_crawler, name='data')
+    # path('all_items/<str:keyword>/', data_crawler, name='data')
 ]
