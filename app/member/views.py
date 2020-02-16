da7889c31bd92f73754c0efbe8106aac6212f30b
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-17 02:11:58 +0900

    login view

--- app/member/views.py
+++ app/member/views.py
@@ -1,19 +1,23 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from member.forms import LoginForm
 
 
 def login_view(request):
     if request.method == 'POST':
-        pass
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            form.login(request)
+
+        return redirect('shop:shopping-list')
     else:
         form = LoginForm()
-        context = {
-            'loginForm': form
-        }
+    context = {
+        'loginForm': form
+    }
     return render(request, 'members/login.html', context)
 
 
 def signup_view(request):
-    pass
+    return render(request, 'members/signup.html')
