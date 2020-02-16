24781d001bb8c3425fad7eb321b6f3326dd455f2
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-16 19:12:14 +0900

    login view for GET method

--- app/member/views.py
+++ app/member/views.py
@@ -1,3 +1,19 @@
+# Create your views here.
 from django.shortcuts import render
 
-# Create your views here.
+from member.forms import LoginForm
+
+
+def login_view(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = LoginForm()
+        context = {
+            'loginForm': form
+        }
+    return render(request, 'members/login.html', context)
+
+
+def signup_view(request):
+    pass
