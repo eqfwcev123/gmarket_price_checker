bca12b6536607466430783ba5899c5911c52c8d0
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-17 21:13:16 +0900

    created signup form

--- app/member/forms.py
+++ app/member/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth import authenticate, login
 from django.core.exceptions import ValidationError
 
+from member.models import User
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="사용자 아이디", widget=forms.TextInput(
@@ -25,8 +27,63 @@ class LoginForm(forms.Form):
             raise ValidationError("아이디 혹은 비밀번호를 잘못 입력하셨 습니다.")
         return self.cleaned_data
 
-    def login(self,request):
+    def login(self, request):
         username = self.cleaned_data['username']
         password = self.cleaned_data['password']
         user = authenticate(request, username=username, password=password)
-        login(request,user)
+        login(request, user)
+
+
+class SignupForm(forms.Form):
+    username = forms.CharField(
+        label='사용자 아이디',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'username-field',
+                'placeholder': '아이디',
+            }
+        ))
+    password = forms.CharField(
+        label='사용자 비밀번호',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'password-field',
+                'placeholder': '비밀번호',
+            }
+        ))
+    email = forms.EmailField(
+        label='사용자 이메일',
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'email-field',
+                'placeholder': '이메일',
+            }
+        ))
+    name = forms.CharField(
+        label='이름',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'name-field',
+                'placeholder': '이름',
+            }
+        ))
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise ValidationError('이미 사용중인 username입니다')
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('이미 사용중인 email입니다')
+        return email
+
+    def save(self):
+        User.objects.create(
+            username=self.cleaned_data['username'],
+            password=self.cleaned_data['password'],
+            email=self.cleaned_data['email'],
+            name=self.cleaned_data['name']
+        )
