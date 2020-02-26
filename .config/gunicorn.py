cb25d4cc786a3453ef2006f5b3b04401fa5da8f7
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-26 20:43:54 +0900

    connected gunicorn.py to gunicorn

---
+++ .config/gunicorn.py
@@ -0,0 +1,6 @@
+daemon = False
+chdir = '/srv/gmarkek_price_checker/app'
+bind = 'unix:/run/gmarket.sock'
+accessing = '/var/log/gunicorn/gmarket-access.log'
+errorlog = '/var/log/gunicorn/gmarket-error.log'
+capture_output = True
