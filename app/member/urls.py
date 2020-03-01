9ca1f201bf101ee9a6ff81dcf9dc5561b1965953
eqfwcev123 <dohyeonee95@hotmail.com>
2020-03-02 03:19:43 +0900

    signout view connected to url members:signout

--- app/member/urls.py
+++ app/member/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from member.views import signup_view, login_view
+from member.views import signup_view, login_view, logout_view
 
 app_name = 'members'
 urlpatterns = [
     path('', signup_view, name='signup'),
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout')
 ]
