0bcf89a7ca32e4c268ac79f5226b8f6143fd7434
eqfwcev123 <dohyeonee95@hotmail.com>
2020-03-02 03:19:17 +0900

    login view completed

--- app/member/views.py
+++ app/member/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from member.forms import LoginForm, SignupForm
 
@@ -8,10 +9,10 @@ def login_view(request):
         form = LoginForm(data=request.POST)
         if form.is_valid():
             form.login(request)
-
-        return redirect('shop:shopping-list')
+            return redirect('shop:shopping-list')
     else:
         form = LoginForm()
+    print(request.user)
     context = {
         'loginForm': form
     }
@@ -28,3 +29,8 @@ def signup_view(request):
         'signupForm': form
     }
     return render(request, 'members/signup.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('members:login')
